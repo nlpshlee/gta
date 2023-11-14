a1e252202b7306b634372663ed1f79bd962dcda8
seokyoung96 <syhong0925@gmail.com>
2023-11-14 22:27:44 +0900

    create branch

--- source/gta/stonezero/tokenize/word_tokenizer.py
+++ source/gta/stonezero/tokenize/word_tokenizer.py
@@ -65,7 +65,7 @@ if __name__ == "__main__" :
     tokenizer = WordTokenizer()
     tokenizer.train(in_file_path, 'UTF-8', '\t', out_file_path)
 
-    text = '지금 우리가 카페에서 공부하고 있는 이유는 앞서 공부를 정말 많이 한 선배들을 어떻게든 따라잡고 나도 한가닥 좀 해보려고 발버둥 치는 것이다.'
+    text = '우리가 공부하고 있는 이유는 앞서 공부를 정말 많이 한 선배들을 어떻게든 따라잡고 나도 한가닥 좀 해보려고 발버둥 치는 것이다.'
     result = tokenizer.tokenize(text, out_file_path)
 
     print(result)
