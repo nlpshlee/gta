e8f5bd1b59e13bdf44470f043ca68a95dbb38eff
nlpshlee <tjdgml0820@naver.com>
2023-11-28 20:56:21 +0900

    add log file setting

--- source/gta/falcon/transformer/commons/tsfr_properties.py
+++ source/gta/falcon/transformer/commons/tsfr_properties.py
@@ -1,6 +1,8 @@
 from _init import *
 
-from gta.falcon.transformer.commons import tsfr_const
+from gta.modules import common_util, file_util
+
+from gta.falcon.transformer.commons import tsfr_const, tsfr_util
 
 
 class TSFRProperties :
@@ -9,16 +11,25 @@ class TSFRProperties :
         self.encoding = tsfr_const.ENCODING
         
         self.model_dir = "models"
+        self.model_path = tsfr_const.MODEL_VERSION
     
     def set(self, res_dir: str, encoding: str, logging_flag: bool) :
         self.res_dir = res_dir
         self.encoding = encoding
         
         self.model_dir = os.path.join(self.res_dir, self.model_dir)
+
+        self.model_path = os.path.join(self.model_dir, self.model_path)
+        file_util.make_parent(os.path.join(self.model_path, "model.tmp"))
+        
+        self.model_log_path = os.path.join(self.model_path, "training.log")
+        if logging_flag :
+            tsfr_util.set_log_file_path(self.model_log_path, self.encoding)
     
     def _print(self) :
-        print(f'\nres_dir : {self.res_dir}')
-        print(f'model_dir : {self.model_dir}\n')
+        tsfr_util.logging(f'\nres_dir : {self.res_dir}')
+        tsfr_util.logging(f'model_dir : {self.model_dir}')
+        tsfr_util.logging(f'model_path : {self.model_path}\n')
 
 
 ''' if __name__ == "__main__" :
